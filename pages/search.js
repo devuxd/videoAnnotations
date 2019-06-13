34aeca6628661fce80f799c3cabcb786c258dbd3
Abdulaziz <alobodi1@hotmail.com>
2019-06-13 11:26:28 -0400

    added annotations

--- pages/search.js
+++ pages/search.js
@@ -1,19 +1,36 @@
 import React from "react";
+import collection from "../API/db";
 
 export default class search extends React.Component {
   static async getInitialProps({ query }) {
-    console.log(`You searched for ${query}`)
-    return {query}
+    console.log(`You searched for ${query}`);
+    return { query };
+  }
+  constructor() {
+    super();
+    this.state = { annotations: [] };
+  }
+  componentDidMount() {
+    let LocalAnnotations = this.searchForAnnotation(
+      this.props.query.annotation
+    );
+    this.setState({ annotations: LocalAnnotations });
   }
   render() {
+    console.log(this.state.annotations);
     return (
       <div>
-     You searched for : {JSON.stringify(this.props.query.annotation)}
-   
+        You searched for : {JSON.stringify(this.props.query.annotation)}
+        <br/>
+        <br/>
+        <br/>
+        Annotations : {JSON.stringify(this.state.annotations)}
+
       </div>
     );
   }
+  searchForAnnotation(annotation) {
+    // Hard coded return values.
+    return collection;
+  }
 }
-
-
- 
