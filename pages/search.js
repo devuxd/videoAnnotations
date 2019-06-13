652270693fb5d4eca70fd00d9c0f1c8a9f0cda21
Ryan <ryanhkim00@gmail.com>
2019-06-13 15:27:09 -0400

    search file missing

---
+++ pages/search.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faAlignCenter } from "@fortawesome/free-solid-svg-icons";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import Router from "next/router";
+import VideoList from "../components/videoList.js";
+import Layouts from "./layouts";
+//import Navigation from '../components/navigation.js';
+// this line causing error: "Module not found"
+// import { createRequireFromPath } from "module";
+import collection from "../API/db";
+
+export default class search extends React.Component {
+  static async getInitialProps({ query }) {
+    console.log(`You searched for ${query}`);
+    return { query };
+  }
+  constructor() {
+    super();
+    this.state = { annotations: [] };
+  }
+  componentDidMount() {
+    let LocalAnnotations = this.searchForAnnotation(
+      this.props.query.annotation
+    );
+    this.setState({ annotations: LocalAnnotations });
+  }
+  render() {
+    console.log(this.state.annotations);
+    return (
+      <div>
+        <Layouts>{VideoList()}</Layouts>
+        You searched for : {JSON.stringify(this.props.query.annotation)}
+        <br/>
+        <br/>
+        <br/>
+        Annotations : {JSON.stringify(this.state.annotations)}
+
+      </div>
+    );
+  }
+  searchForAnnotation(annotation) {
+    // Hard coded return values.
+    return collection;
+  }
+}
