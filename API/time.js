d1b0bdcc9f41809f123aba75d03776f3a054a8d5
Abdulaziz <alobodi1@hotmail.com>
2020-03-04 10:24:38 -0500

    fixed time formate

--- API/time.js
+++ API/time.js
@@ -1,9 +1,16 @@
 import moment from "moment";
 
-const secondsToStringFormat = seconds => {
-  const hours = Math.floor(moment.duration(seconds, "seconds").asHours());
-  const time = moment.duration(seconds, "seconds");
-  return `${hours}:${time.minutes()}:${time.seconds()}`;
+const secondsToStringFormat = TotalTimeInSeconds => {
+  // this is done to get hours after 24. Moment convert 24 hours to a day in duration
+  const hours = Math.floor(
+    moment.duration(TotalTimeInSeconds, "seconds").asHours()
+  );
+  // this is to get the minutes and seconds
+  const time = moment.duration(TotalTimeInSeconds, "seconds");
+  // formating
+  const minutes = time.minutes() > 9 ? time.minutes() : `0${time.minutes()}`;
+  const seconds = time.seconds() > 9 ? time.seconds() : `0${time.seconds()}`;
+  return `${hours}:${minutes}:${seconds}`;
 };
 
 const stringToSecondsFormat = string => moment.duration(string).asSeconds();
